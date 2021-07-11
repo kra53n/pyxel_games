104f12c25587e687e2443623b1edf25c4410f521
Krai53n <greasha46@gmail.com>
2021-07-11 08:46:49 +0300

    elements

--- ping_pong/ping_pong/__main__.py
+++ ping_pong/ping_pong/__main__.py
@@ -1,6 +1,7 @@
 import pyxel
-
 from screen_resolution import get_monitor_resolution
+
+from elements import Stick
 
 
 SCREEN_WIDTH, SCREEN_HEIGHT = [i // 10 for i in get_monitor_resolution()]
@@ -11,9 +12,10 @@ SCREEN_FPS = 60
 STICK_WIDTH = 2
 STICK_HEIGHT = 30
 STICK_MARGIN = 3
+STICK_MOVE_STEP = 1
 
 COLOR_STICK_PLAYER = 8
-COOR_STICK_ENEMY = 8
+COLOR_STICK_ENEMY = 8
 
 KEY_START = pyxel.KEY_ENTER
 
@@ -30,6 +32,17 @@ class App:
             fullscreen=True,
         )
 
+        self.stick_player = Stick(
+            x=STICK_MARGIN,
+            y=(SCREEN_HEIGHT - STICK_HEIGHT) // 2,
+            w=STICK_WIDTH,
+            h=STICK_HEIGHT,
+            col=COLOR_STICK_PLAYER,
+            move_step=STICK_MOVE_STEP,
+            screen_h=SCREEN_HEIGHT,
+            screen_w=SCREEN_WIDTH,
+        )
+
         pyxel.run(self.update, self.draw)
     
     def update(self):
@@ -37,11 +50,12 @@ class App:
 
     def draw(self):
         pyxel.cls(1)
-        self.draw_stick(STICK_MARGIN, 0)
-        self.draw_stick(SCREEN_WIDTH - STICK_MARGIN - STICK_WIDTH, 0)
+        self.stick_player.draw()
 
-    def draw_stick(self, x, y, color=COLOR_STICK_PLAYER):
-        pyxel.rect(x, y, STICK_WIDTH, STICK_HEIGHT, color)
+        if pyxel.btn(pyxel.KEY_W):
+            self.stick_player.up()
+        if pyxel.btn(pyxel.KEY_S):
+            self.stick_player.down()
 
 
 if __name__ == "__main__":
---
+++ ping_pong/ping_pong/elements.py
@@ -0,0 +1,47 @@
+import pyxel
+
+
+class Element:
+    def __init__(self, col, move_step, screen_h, screen_w):
+        self.col = col
+        self.move_step = move_step
+        self.screen_h = screen_h
+        self.screen_w = screen_w
+
+    @property
+    def begin(self):
+        return self.x
+
+
+class Stick(Element):
+    def __init__(self, x, y, w, h, **kwargs):
+        # https://tirinox.ru/super-python/
+        super().__init__()
+
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    def draw(self):
+        pyxel.rect(self.x, self.y, self.w, self.h, self.col)
+
+    @property
+    def end(self):
+        return self.x + self.h
+    
+    def up(self):
+        if self.y > 0:
+            self.y = self.y - self.move_step
+
+    def down(self):
+        if self.y + self.h < self.screen_h:
+            self.y = self.y + self.move_step
+
+
+class Ball(Element):
+    def __init__(self, r):
+        super().__init__()
+
+    def draw(self):
+        pyxel.circ(self.x, self.y,)
