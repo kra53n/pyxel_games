38a3626ca4baeba04ae470757094dd4754a42d9e
Krai53n <greasha46@gmail.com>
2022-10-22 21:25:04 +0300

    add history

--- editor/16bit_editor.py
+++ editor/16bit_editor.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import pyxel as px
 import png
 
@@ -15,12 +17,59 @@ def load_palette() -> list:
     return [(h // 256**2, h // 256 % 256, h % 256) for h in px.colors]
 
 
+@dataclass
+class HistoryElem:
+    x: int = 0
+    y: int = 0
+    prv_col: int = 0
+    nxt_col: int = 0
+
+    def __iter__(self):
+        elems = self.x, self.y, self.prv_col, self.nxt_col
+        for elem in elems:
+            yield elem
+
+    def __eq__(self, other):
+        if type(other) != HistoryElem:
+            return
+        return all((self.x == other.x,
+                    self.y == other.y,
+                    self.prv_col == other.prv_col,
+                    self.nxt_col == other.nxt_col))
+
+
+class History:
+    def __init__(self):
+        self._stack = []
+        self._idx = 0
+
+    def add(self, x: int, y: int, img: int, col: int):
+        elem = HistoryElem(x, y, img[y][x], col)
+        print(self._stack)
+        if elem in self._stack:
+            return
+        self._idx += 1
+        self._stack = self._stack[:self._idx]
+        self._stack.append(elem)
+
+    def undo(self, img: list):
+        if self._idx > 0:
+            self._idx -= 1
+            x, y, col, _ = self._stack[self._idx]
+            img[y][x] = col
+
+    def recover(self, img: list):
+        if self._idx < len(self._stack) - 1:
+            self._idx += 1
+            x, y, _, col = self._stack[self._idx]
+            img[y][x] = col
+
 class Image:
     def __init__(self):
         self._w = SCREEN_W
         self._h = SCREEN_H
         self._pixels = [[0] * self._w for _ in range(self._h)]
-        self._history = []
+        self._history = History()
 
     def save(self):
         filename = f'{(SCREEN_W + SCREEN_H) // 2}_bit editor.png'
@@ -39,11 +88,17 @@ class Image:
         col = kw['col']
 
         if px.btn(px.MOUSE_BUTTON_LEFT):
+            self._history.add(px.mouse_x, px.mouse_y, self._pixels, col)
             self._pixels[px.mouse_y][px.mouse_x] = col
         if px.btnp(px.KEY_C):
             self._clear(col)
         if px.btnp(px.KEY_S):
             self.save()
+
+        if px.btnp(px.KEY_Z):
+            self._history.undo(self._pixels)
+        if px.btnp(px.KEY_X):
+            self._history.recover(self._pixels)
 
     def draw(self):
         for x in range(self._w):
